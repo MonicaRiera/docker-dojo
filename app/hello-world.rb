9f2e74427c6ab9a8d8cf50ca61efe38bfa9e0d7b
Monica <monica.riera@bts.tech>
2019-03-09 11:41:06 +0100

    Redis

--- app/hello-world.rb
+++ app/hello-world.rb
@@ -7,7 +7,7 @@ set :bind, '0.0.0.0'
 redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
 
 get '/' do
-   redis.ping
+   #redis.ping
 
   "Hello Monica!!!!!!!"
 end

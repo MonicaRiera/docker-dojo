ae0db22295e07cd927124d7dfe0bee1f0e703509
Monica <monica.riera@bts.tech>
2019-03-09 11:11:11 +0100

    Docker exercise

--- app/hello-world.rb
+++ app/hello-world.rb
@@ -7,8 +7,8 @@ set :bind, '0.0.0.0'
 redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDIS_PORT"] || 6379)
 
 get '/' do
-  redis.ping
+   #redis.ping
 
-  "Hello World!"
+  "Hello Monica!!!!!!!"
 end
  
